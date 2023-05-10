03fc30296952fe6a678c355680341d5ae868ff7c
ddsal07 <danidiaz0799@gmail.com>
2023-05-10 13:09:33 -0500

    implementando servicio

--- src/app/components/doctor/doctor.component.ts
+++ src/app/components/doctor/doctor.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-doctor',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./doctor.component.css']
 })
 export class DoctorComponent {
+
+  loginForm! : FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router){}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      especialidad: ['', Validators.required],
+      consultorio: ['', Validators.required],
+      correo: ['', Validators.required]
+    })
+  }
 
 }
