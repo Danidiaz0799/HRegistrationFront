cfbabbbcd4a73e3426e68bc362d3b97bc8f319f6
ddsal07 <danidiaz0799@gmail.com>
2023-05-25 02:26:18 -0500

    implementacion de doctores por especialidad

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Doctor } from 'src/app/models/doctor.interface';
+import { DoctorServiceService } from 'src/app/services/doctor-service.service';
+import { PatientServiceService } from 'src/app/services/patient-service.service';
 
 @Component({
   selector: 'app-appointment',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment.component.css']
 })
 export class AppointmentComponent {
+
+  doctors$: Observable<Doctor[]> = new Observable<Doctor[]>();
+
+  constructor(public doctorService: DoctorServiceService, public patientService: PatientServiceService) { }
+
+  ngOnInit() {
+    this.getDoctorsBySpecialtie("General medicine");
+  }
+
+  getDoctorsBySpecialtie(Specialtie: string): void {
+    this.doctorService.getDoctorsBySpecialtie(Specialtie).subscribe(
+      (doctors: Doctor[]) => {
+        console.log('Doctores:', doctors); // Agregar console.log para mostrar los doctores
+        this.doctors$ = new Observable<Doctor[]>(observer => {
+          observer.next(doctors);
+          observer.complete();
+        });
+      },
+      (error: any) => {
+        console.error('Error al obtener los médicos por especialidad:', error);
+      }
+    );
+  }
 
 }
